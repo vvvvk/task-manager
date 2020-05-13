ec76048069238d135f060db5264c32c2d3adba31
Vasiliy Kryuchkov <vvvvk@yandex.ru>
2020-05-13 12:02:41 +0400

    session

--- test/controllers/web/sessions_controller_test.rb
+++ test/controllers/web/sessions_controller_test.rb
@@ -6,4 +6,20 @@ class Web::SessionsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should post create" do
+    password = generate(:string)
+    user = create(:user, { password: password })
+    attrs = {
+      email: user.email,
+      password: password
+    }
+    post :create, params: {session_form: attrs }
+    assert_response :redirect
+  end
+
+  test "should delete destroy" do
+    delete :destroy
+    assert_response :redirect
+  end
+
 end
